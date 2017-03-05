aa59c60f0acee9e6be1a19b93e480dc72ff9e46d
rezanrezano <captenrez@gmail.com>
2017-03-06 01:23:07 +0300

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm(){
+	$('form').on('submit',function(e){
+		var items=$('#item').val();;
+		$('ol').append('<li>'+items+'</li>');
+		
+		e.preventDefault();
+	});
+}
