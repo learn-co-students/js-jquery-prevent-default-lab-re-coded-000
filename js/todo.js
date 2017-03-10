e4e9be7053569d4ea1e161c0c896ec7f195839b9
Saber Rasheed <edrees.rasheed@yahoo.com>
2017-03-10 21:00:33 +0300

    Done.

--- js/todo.js
+++ js/todo.js
@@ -3,14 +3,6 @@ $(document).ready(function(){
   submitForm();
 });
 
-// define functions here
-
-function submitForm(){
-	
-
-	$('form').on('submit', function(event){		
-        var listitem = $('input').val();
-		$('#list ol').append('<li>' + listitem + '</li>');
-		event.preventDefault();
-	})
-}
+// define functions her
+function submitForm() {$('form').on('submit', function(event){add();event.preventDefault();  });}
+function add() {$('#list ol').append("<li>"+$('#item').val()+"</li>");$('#item').val("")}
